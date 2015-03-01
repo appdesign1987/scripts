db961267931e124617994be9ac9932f5823a1ead
appdesign1987 <jeroen@jeroenvd.nl>
2015-03-01 12:02:02 +0100

    Update setuplamernews.sh

--- setuplamernews.sh
+++ setuplamernews.sh
@@ -2,3 +2,5 @@
 
 docker pull redis
 docker run --name redisserver -d redis
+docker build -t jeroen/lamernewsapp .
+docker run -d -P -p 0.0.0.0:80:4567 -i -t --name lamernews --link redisserver:redis jeroen/lamernewsapp
