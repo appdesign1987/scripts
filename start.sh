87b370adf446d4e4e6993ea07b317706991005cd
appdesign1987 <jeroen@jeroenvd.nl>
2015-03-01 23:17:13 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,9 +6,6 @@ cd /app/data && git clone https://github.com/appdesign1987/lamernews.git
 #See if theres some new data on github
 #cd /app/data/lamernews && git pull https://github.com/appdesign1987/lamernews.git
 
-#Running bundle
-cd /app/data/lamernews && bundle install
-
 #installing gems
 gem install bundler
 gem install sinatra -v 1.4.2
@@ -16,6 +13,9 @@ gem install redis -v 3.0.4
 gem install hiredis -v 0.4.5
 gem install json -v 1.7.7
 gem install ruby-hmac -v 0.4.0
+
+#Running bundle
+cd /app/data/lamernews && bundle install
 
 #starting application
 cd /app/data/lamernews && bundle exec rackup -p4567 --host 0.0.0.0
