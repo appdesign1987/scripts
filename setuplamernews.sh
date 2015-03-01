8402415b301b5868239fcf13efd2e723b68bc8c6
appdesign1987 <jeroen@jeroenvd.nl>
2015-03-01 16:35:43 +0100

    Update setuplamernews.sh

--- setuplamernews.sh
+++ setuplamernews.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-docker pull redis
-docker run --name redisserver -d redis
 docker create -v /gems/lamernews/ --name gems-lamernews busybox:latest
 docker create -v /app --name lamernewsdata busybox:latest
+docker create -v /data --name redisdata busybox:latest
+docker pull redis
+docker run --name redisserver -volumes-from redisdata -d redis redis-server --appendonly yes
 wget https://files.jeroenvd.nl/files/Dockerfiles/Dockerfile
 docker build -t jeroen/lamernewsapp .
-docker run --volumes-from gems-lamernews --volumes-from lamernewsdata -d -P -p 0.0.0.0:80:4567 -i -t --name lamernews --link redisserver:redis jeroen/lamernewsapp
+docker run --volumes-from gems-lamernews --volumes-from lamernewsdata -d -P -p 0.0.0.0:8080:4567 -i -t --name lamernews --link redisserver:redis jeroen/lamernewsapp
