f175bf15df2e04ff7c73502879daa8e8f0370076
appdesign1987 <jeroen@jeroenvd.nl>
2015-03-01 22:41:40 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 #Git clone app
-cd /data/app && git clone https://github.com/appdesign1987/lamernews.git
+cd /app/data && git clone https://github.com/appdesign1987/lamernews.git
 
 #See if theres some new data on github
-cd /data/app/lamernews && git pull https://github.com/appdesign1987/lamernews.git
+cd /app/data/lamernews && git pull https://github.com/appdesign1987/lamernews.git
 
 #Running bundle
-cd /data/app/lamernews && bundle install
+cd /app/data/lamernews && bundle install
 
 #installing gems
 gem install bundler
@@ -18,4 +18,4 @@ gem install json -v 1.7.7
 gem install ruby-hmac -v 0.4.0
 
 #starting application
-cd /data/app/lamernews && bundle exec rackup -p4567 --host 0.0.0.0
+cd /app/data/lamernews && bundle exec rackup -p4567 --host 0.0.0.0
