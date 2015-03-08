c9068462ff84aba5d7ffd33108e3f29be96c1c0d
appdesign1987 <jeroen@jeroenvd.nl>
2015-03-08 22:32:09 +0100

    Update startSeafile.sh

--- startSeafile.sh
+++ startSeafile.sh
@@ -1,28 +1,8 @@
 #!/usr/bin/env bash
 
-if [ -d "/srv/seafile/seafile-server-4.0.6" ]; then
-#start Seafile and Seahub
-cd /srv/seafile/seafile-server-latest/ && ./seahub.sh start-fastcgi
-cd /srv/seafile/seafile-server-latest/ && ./seafile.sh start
-
-#Start Apache
-/usr/sbin/apache2 -D FOREGROUND
-
-fi
-
-# Download new seafile-version
-cd /tmp && wget https://bitbucket.org/haiwen/seafile/downloads/seafile-server_4.0.6_x86-64.tar.gz
-
-#Extract downloaded version
-cd /tmp && tar zxvf seafile-server*.tar.gz
-
-#mkdir Seafile folder 
-mkdir /srv/seafile/
-
-#move extracted files to Seafile folder
-cd /tmp && mv seafile-server* /srv/seafile/
-
-#Create symlink to new latest seafile-server
-ln -s /srv/seafile/seafile-server-4.0.6 /srv/seafile/seafile-server-latest
-
-
+#download setup file
+cd /tmp && wget http://files.jeroenvd.nl/files/scripts/setupseafile.sh
+#set execute script
+chmod +x /tmp/setupseafile.sh
+#start setup script
+cd /tmp && ./setupseafile.sh
