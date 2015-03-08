f19b04d438a8528761292ec4a082c59c9e67eb8d
appdesign1987 <jeroen@jeroenvd.nl>
2015-03-08 11:30:44 +0100

    Update startPhpmyadmin.sh

--- startPhpmyadmin.sh
+++ startPhpmyadmin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #Git clone Wordpress
-cd /var/www && git clone https://github.com/phpmyadmin/phpmyadmin.git
+cd /var/www && git clone --branch stable https://github.com/phpmyadmin/phpmyadmin.git
 
 # Start apache
 /usr/sbin/apache2 -D FOREGROUND
