5f6db8a3a3abf6df9eb2e187645d75b35eb4c7ca
appdesign1987 <jeroen@jeroenvd.nl>
2015-03-08 23:05:18 +0100

    Update startSeafile.sh

--- startSeafile.sh
+++ startSeafile.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -d "$DIRECTORY" ]; then
-
-#Start seafile
-cd /srv/seafile/seafile-server-latest && ./seafile.sh start
-cd /srv/seafile/seafile-server-latest && ./seahub.sh start-fastcgi
-
-# Start apache
-/usr/sbin/apache2 -D FOREGROUND
-
-fi
-
-
 #download setup file
 cd /tmp && wget http://files.jeroenvd.nl/files/scripts/setupseafile.sh
 #set execute script
