f2dbcfd59896f58bb520df0a47cbb4c5c500190d
appdesign1987 <jeroen@jeroenvd.nl>
2015-03-08 18:53:18 +0100

    Update startSeafile.sh

--- startSeafile.sh
+++ startSeafile.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 #start Seafile and Seahub
+cd /srv/seafile/seafile-server-latest/ && ./seahub.sh start-fastcgi
+cd /srv/seafile/seafile-server-latest/ && ./seafile.sh start
 
 #Start Apache
-
+/usr/sbin/apache2 -D FOREGROUND
