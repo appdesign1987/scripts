a16c60d205f0be3c7b47aa387b30d392fd2e87f8
appdesign1987 <jeroen@jeroenvd.nl>
2015-03-14 10:00:41 +0100

    Create mailinabox.sh

---
+++ mailinabox.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#########################################################
+# This script is intended to be run like this:
+#
+#   curl https://.../mailinabox.sh | sudo bash
+#
+#########################################################
+
+if [ -z "$TAG" ]; then
+        TAG=v0.07
+fi
+
+# Are we running as root?
+if [[ $EUID -ne 0 ]]; then
+        echo "This script must be run as root. Did you leave out sudo?"
+        exit
+fi
+
+# Clone the Mail-in-a-Box repository if it doesn't exist.
+if [ ! -d $HOME/mailinabox ]; then
+        echo Installing git . . .
+        DEBIAN_FRONTEND=noninteractive apt-get -q -q install -y git < /dev/null
+        echo
+
+        echo Downloading Mail-in-a-Box. . .
+        git clone \
+                --depth 1 \
+                https://github.com/appdesign1987/mailinabox \
+                $HOME/mailinabox \
+                < /dev/null 2> /dev/null
+
+        echo
+fi
+
+# Change directory to it.
+cd $HOME/mailinabox
+
+# Update it.
+if [ "$TAG" != `git describe` ]; then
+        echo Updating Mail-in-a-Box to . . .
+        git fetch --depth 1 --force --prune origin
+        if ! git checkout -q; then
+                echo "Update failed. Did you modify something in `pwd`?"
+                exit
+        fi
+        echo
+fi
+
+# Start setup script.
+setup/start.sh
