478ebf6363c39543cccd16b75bd0ad0db786f4dc
appdesign1987 <jeroen@jeroenvd.nl>
2015-03-15 20:14:15 +0100

    Update startBaikal.sh

--- startBaikal.sh
+++ startBaikal.sh
@@ -7,10 +7,10 @@ cd /var/www && wget http://baikal-server.com/get/baikal-regular-0.2.7.tgz
 cd /var/www/ && tar xzf baikal-regular-0.2.7.tgz
 
 #mv extracted data from archived folder to root
-cd /var/www/baikal-0.2.7 && mv -Rf * ../
+cd /var/www/baikal-regular && mv -Rf * ../
 
 #remove extracted directory
-rm -R /var/www/baikal-0.2.7
+rm -R /var/www/baikal-regular
 
 #remove default apache2 config
 rm /etc/apache2/sites-enabled/default*
