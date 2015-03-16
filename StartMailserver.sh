f71f4db7db8011e7a4ab94538c7895ac0afb3444
appdesign1987 <jeroen@jeroenvd.nl>
2015-03-16 15:28:07 +0100

    Update StartMailserver.sh

--- StartMailserver.sh
+++ StartMailserver.sh
@@ -9,7 +9,8 @@
 cd /var/www && git clone git://github.com/opensolutions/ViMbAdmin.git
 cd /var/www/ViMbAdmin && mv -f * ../
 
-
+#Set folder rights once more
+cd /var/ && chown -R www-data:www-data www
 
 # Start apache
 /usr/sbin/apache2 -D FOREGROUND
