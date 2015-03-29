da937b6e109a06adb9ff370b71fd64d9f59fca69
Jeroen van Drongelen <jeroenvandrongelen@funda.nl>
2015-03-29 13:56:32 +0200

    Fixed a few things so it does hopefully now

--- Backup.sh
+++ Backup.sh
@@ -7,16 +7,14 @@ mkdir /duplicity
 cd /duplicity && git clone https://github.com/zertrin/duplicity-backup.git
 cd /duplicity/duplicity-backup && mv * ../
 cd /duplicity && rm -R duplicity-backup
-
-
+chmod +x /duplicity/duplicity-backup.sh
 
 # check if we need to do a restore if not then check if duplicity-backup.conf exist
-# if restore is yes we are going to skip al of this and restore the files.
-
-
+# if restore is yes we are going to skip all of this and restore the files.
 if [ $restore -eq 0 ]; then
-	if [ -f /etc/duplicity-backup.conf ] then
+	if [ -f /duplicity/duplicity-backup.conf ] then
      	echo the config file exist we are going to start the backup
+     	while true; do sh /duplicity/duplicity-backup.sh --backup; sleep h$hours ;done
 	else
     	echo the config file does not exist we are going to make it and make cron options
     #create config file
@@ -25,20 +23,24 @@ if [ $restore -eq 0 ]; then
 		AWS_SECRET_ACCESS_KEY="$AWS_SECRET_KEY"
 		ENCRYPTION='no'
 		ROOT="$Backupfolder"
-		DEST="s3+http://cureosuserdata.s3.amazonaws.com/$Backupfolder"
+		DEST="s3+http://s3.amazonaws.com/$bucket/$Backupfolder"
 		STATIC_OPTIONS="--full-if-older-than 14D --s3-use-new-style --s3-european-buckets"
 		CLEAN_UP_TYPE="remove-all-but-n-full"
 		CLEAN_UP_VARIABLE="4"
 EOL
  
-		#set our cronjob to make it run every x hours
-
+		#set our cronjob ot something to make it run every x hours
+		#crontab -l > backupjob
+		#echo "0 */$hours * * * sh /duplicity/duplicity-backup.sh --backup --full" >> backupjob
+		#install cron job
+		#crontab backupjob
+		#rm backupjob
+		while true; do sh /duplicity/duplicity-backup.sh --backup; sleep h$hours ; done
 	fi
 
 else
-#So the restore eq value said it's not 0 so we are going to do a restore!
-
-
+#So the restore eq value said it's not 0 so we are going to do a restore! and make a cronjob afterwarts
+sh /duplicity/duplicity-backup.sh --restore $Backupfolder
 fi
 
 
