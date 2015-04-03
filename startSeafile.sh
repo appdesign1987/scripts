dc672248fdd96a05ba5443263f1007ac7a541aaf
appdesign1987 <jeroen@jeroenvd.nl>
2015-04-03 19:51:26 +0200

    Update startSeafile.sh

--- startSeafile.sh
+++ startSeafile.sh
@@ -1,8 +1,47 @@
 #!/usr/bin/env bash
+#EDIT THE FOLLOWING OPTIONS ACCORDING THE RIGHT VERSION
 
-#download setup file
-cd /tmp && wget http://files.jeroenvd.nl/files/scripts/setupseafile.sh
-#set execute script
-chmod +x /tmp/setupseafile.sh
-#start setup script
-cd /tmp && ./setupseafile.sh
+#set version of seafile
+export seafileversion=4.0.6
+
+#set $directory
+export directory=/srv/seafile/seafile-server-$seafileversion
+
+#DO NOT TOUCH BELOW THIS LINE
+
+if [ -d "$DIRECTORY" ]; then
+  
+  #start seafile
+  cd /srv/seafile/seafile-server-latest && ./seafile.sh start
+  
+  #start seafileserver hub 
+  cd /srv/seafile/seafile-server-latest && ./seahub.sh start-fastcgi
+  
+ # Start apache
+/usr/sbin/apache2 -D FOREGROUND
+  
+fi
+
+#remove symlink
+rm /srv/seafile/seafile-server-latest
+
+#download latest seafile-server version
+cd /tmp && wget https://bitbucket.org/haiwen/seafile/downloads/seafile-server_$seafileversion_x86-64.tar.gz
+
+#extract downloaded seafile-server-version
+cd /tmp && tar zxvf seafile-server_*.tar.gz
+
+#mv seafileserver version to installation folder
+cd /tmp && mv seafile-server-$seafileversion /srv/seafile/
+
+#create symlink
+ln -s /srv/seafile/seafile-server-latest /srv/seafile/seafile-server-$seafileversion
+
+#start seafile
+  cd /srv/seafile/seafile-server-latest && ./seafile.sh start
+  
+  #start seafileserver hub 
+  cd /srv/seafile/seafile-server-latest && ./seahub.sh start-fastcgi
+  
+# Start apache
+/usr/sbin/apache2 -D FOREGROUND
