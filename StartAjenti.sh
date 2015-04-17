c03964ff990ba25f73a0a8bc0c728d7ce9c04053
appdesign1987 <jeroen@jeroenvd.nl>
2015-04-17 21:02:34 +0200

    Update StartAjenti.sh

--- StartAjenti.sh
+++ StartAjenti.sh
@@ -12,6 +12,22 @@
 apt-get update
 apt-get -y upgrade
 
+#creating symlinks and moving folder to keep everything persistent
+#mv /var/vmail /data/vmail
+#ln -s /data/vmail /var/vmail
+#mv /var/lib/mysql /data/mysql
+#ln -s /data/mysql /var/lib/mysql
+#mv /backup /data/backup
+#ln -s /data/backup /backup
+#mv /etc/nginx /data/nginx
+#ln -s /data/nginx /etc/nginx
+#mv /etc/pure-ftpd /data/pureftpd
+#ln -s /data/pureftpd /etc/pure-ftpd
+#mv /etc/ajenti /data/ajenti-config
+#ln -s /data/ajenti-config /etc/ajenti
+
+
+
 #En nu gaan we ajenti starten
 cd /etc/init.d/ && ./ajenti stop
 /usr/bin/ajenti-panel -v
