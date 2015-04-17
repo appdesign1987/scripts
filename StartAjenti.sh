5e080dfe3de1480af30fb99fe1237ef84031dc5b
appdesign1987 <jeroen@jeroenvd.nl>
2015-04-17 21:17:17 +0200

    Update StartAjenti.sh

--- StartAjenti.sh
+++ StartAjenti.sh
@@ -12,22 +12,25 @@
 apt-get update
 apt-get -y upgrade
 
+#we gaan eerst ajenti stoppen om alles te kunnen verplaatsen
+#cd /etc/init.d/ && ./ajenti stop && ./nginx stop && ./exi
+
+
 #creating symlinks and moving folder to keep everything persistent
-#mv /var/vmail /data/vmail
-#ln -s /data/vmail /var/vmail
-#mv /var/lib/mysql /data/mysql
-#ln -s /data/mysql /var/lib/mysql
-#mv /backup /data/backup
-#ln -s /data/backup /backup
-#mv /etc/nginx /data/nginx
-#ln -s /data/nginx /etc/nginx
-#mv /etc/pure-ftpd /data/pureftpd
-#ln -s /data/pureftpd /etc/pure-ftpd
-#mv /etc/ajenti /data/ajenti-config
-#ln -s /data/ajenti-config /etc/ajenti
-
-
+mv /var/vmail /data/vmail
+ln -s /data/vmail /var/vmail
+mv /var/lib/mysql /data/mysql
+ln -s /data/mysql /var/lib/mysql
+mv /backup /data/backup
+ln -s /data/backup /backup
+mv /etc/nginx /data/nginx
+ln -s /data/nginx /etc/nginx
+mv /etc/pure-ftpd /data/pureftpd
+ln -s /data/pureftpd /etc/pure-ftpd
+mv /etc/ajenti /data/ajenti-config
+ln -s /data/ajenti-config /etc/ajenti
+mv /etc/exim4 /data/exim4
+ln -s /data/exim4 /etc/exim4
 
 #En nu gaan we ajenti starten
-cd /etc/init.d/ && ./ajenti stop
 /usr/bin/ajenti-panel -v
