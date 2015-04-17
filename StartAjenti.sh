2c785644593ebe7fc142f8075b960f6974ae3286
appdesign1987 <jeroen@jeroenvd.nl>
2015-04-17 20:54:05 +0200

    Update StartAjenti.sh

--- StartAjenti.sh
+++ StartAjenti.sh
@@ -12,36 +12,6 @@
 apt-get update
 apt-get -y upgrade
 
-#install ajenti
-wget -O- https://raw.github.com/Eugeny/ajenti/master/scripts/install-ubuntu.sh | sudo sh
-
-#fix to get pure-ftpd working
-
-# install package building helpers
-apt-get -y --force-yes install dpkg-dev debhelper
-
-# install dependancies
-apt-get -y build-dep pure-ftpd
-
-# build from source
-mkdir /tmp/pure-ftpd/ && \
-    cd /tmp/pure-ftpd/ && \
-    apt-get source pure-ftpd && \
-    cd pure-ftpd-* && \
-    sed -i '/^optflags=/ s/$/ --without-capabilities/g' ./debian/rules && \
-    dpkg-buildpackage -b -uc
-    
-# install the new deb files
-dpkg -i /tmp/pure-ftpd/pure-ftpd-common*.deb
-apt-get -y install openbsd-inetd
-dpkg -i /tmp/pure-ftpd/pure-ftpd_*.deb
-
-# Prevent pure-ftpd upgrading
-apt-mark hold pure-ftpd pure-ftpd-common
-
-#install Ajenti the control panel
-apt-get -y install ajenti-v ajenti-v-mail ajenti-v-ftp-pureftpd ajenti-v-php-fpm ajenti-v-nginx ajenti-v-mysql
-
 #En nu gaan we ajenti starten
 cd /etc/init.d/ && ./ajenti stop
 /usr/bin/ajenti-panel -v
