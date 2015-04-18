161a220d5cfd18ade73d107a9b1fe1e8409219a3
appdesign1987 <jeroen@jeroenvd.nl>
2015-04-18 22:25:35 +0200

    Update StartAjenti.sh

--- StartAjenti.sh
+++ StartAjenti.sh
@@ -14,20 +14,28 @@ apt-get -y upgrade
 
 #creating symlinks and moving folder to keep everything persistent
 mv /var/vmail /data/vmail
+rm -R /var/vmail
 ln -s /data/vmail /var/vmail
 mv /var/lib/mysql /data/mysql
+rm -R /var/lib/mysql
 ln -s /data/mysql /var/lib/mysql
 mv /backup /data/backup
+rm -R /backup
 ln -s /data/backup /backup
 mv /etc/nginx/conf.d /data/nginx/conf.d
+rm -R /etc/nginx/conf.d
 ln -s /data/nginx/conf.d /etc/nginx/conf.d
 mv /etc/pure-ftpd /data/pureftpd
+rm -R /etc/pure-ftpd
 ln -s /data/pureftpd /etc/pure-ftpd
 mv /etc/ajenti /data/ajenti-config
+rm -R /etc/ajenti
 ln -s /data/ajenti-config /etc/ajenti
 mv /etc/exim4 /data/exim4
+rm -R /etc/exim4
 ln -s /data/exim4 /etc/exim4
 mv /srv /data/www
+rm -R /srv
 ln -s /data/www /srv
 
 #php5-fpm starten we ook
