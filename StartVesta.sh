44485eef479a74e9682a899afa4ed79e0768f98f
appdesign1987 <jeroen@jeroenvd.nl>
2015-04-30 07:24:33 +0200

    Update StartVesta.sh

--- StartVesta.sh
+++ StartVesta.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 apt-get update && apt-get upgrade -y
+echo moving everything to persistent location
 mv /usr/local/vesta /persistent
 ln -s /persistent/vesta /usr/local/vesta
 
+echo starting everything
 cd /etc/init.d/ && ./vesta start
 cd /etc/init.d/ && ./mysql start
 cd /etc/init.d/ && ./nginx start
@@ -11,3 +13,6 @@ cd /etc/init.d/ && ./exim4 start
 cd /etc/init.d/ && ./apache2 start
 cd /etc/init.d/ && ./bind9 start
 cd /etc/init.d/ && ./fail2ban start
+
+#ugly hack to make sure the script will not exit.
+/sbin/my_init
