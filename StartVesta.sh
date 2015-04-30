557dbe420ed0f6fc8b9c18e64f9f7288bd24842e
appdesign1987 <jeroen@jeroenvd.nl>
2015-04-30 07:11:14 +0200

    Update StartVesta.sh

--- StartVesta.sh
+++ StartVesta.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
 
+apt-get update && apt-get upgrade -y
+mv /usr/local/vesta /persistent
+ln -s /persistent/vesta /usr/local/vesta
+
+cd /etc/init.d/ && ./vesta start
+cd /etc/init.d/ && ./mysql start
+cd /etc/init.d/ && ./nginx start
+cd /etc/init.d/ && ./exim4 start
+cd /etc/init.d/ && ./apache2 start
+cd /etc/init.d/ && ./bind9 start
+cd /etc/init.d/ && ./fail2ban start
