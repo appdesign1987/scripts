36b4bf473d5c3d2aaa2635d14029c4abeaa24502
appdesign1987 <jeroen@jeroenvd.nl>
2016-01-04 07:43:17 +0100

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -4,6 +4,8 @@
 cd /tmp && rm -Rf ApacheConfig
 cd /tmp && git clone https://github.com/appdesign1987/ApacheConfig.git
 cd /tmp/ApacheConfig/ && mv *.conf /etc/apache2/sites-enabled/
+cd /etc/apache2/ && rm ports.conf
+cd /etc/apache2/sites-enabled/ && mv ports.conf ../
 
 # Start apache
 cd /etc/init.d/ && ./apache2 restart
